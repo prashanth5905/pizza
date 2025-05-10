8a66e5ed921af72d0e8576502f679babc75977d2
prashanth5905 <p.rachamalla@iitg.ac.in>
2025-05-10 17:39:15 +0530

    menu done

---
+++ p3/src/pages/main.js
@@ -0,0 +1,18 @@
+import './main.css'
+import { useNavigate } from 'react-router-dom';
+const Main = () => {
+    const navigate = useNavigate();
+    return(
+        <>
+            <div className='message'>
+                <div >
+                    <p className='main_message'>Hey there! Hungry? You’re in the right place.</p>
+                    <div className='sub_message'><p>Your cravings just found their favorite place.Freshly baked, perfectly topped, and<br></br> made with love.</p></div>
+                    <p>Order now and let the feast begin! 🍕🔥</p>
+                    <button className='order' onClick={() => navigate('/menu')}>START ORDERING</button>
+                </div>
+            </div>
+        </>
+    );
+}
+export default Main;
